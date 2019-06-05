3c986cad61211b61e7ae816c722a8904f632fbdb
zhou <2891128968@qq.com>
2019-06-05 21:38:39 +0800

    2019.6.5 21:38 远程服务调用的熔断器监控

--- consumer/src/main/java/com/qwerty/springboot/remote/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/qwerty/springboot/remote/consumer/ConsumerApplication.java
@@ -1,15 +1,40 @@
 package com.qwerty.springboot.remote.consumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+	/*
+	* 出现错误：Unable to connect to Command Metric Stream.
+	* 原因：缺少下面这个servlet，springboot1.5的时候这个servlet
+	*      在源码中是配置到spring容器中的，但是2.0的时候没有了，
+	*      所以我们自己手动配置一下即可
+	* */
+	@Bean
+	public ServletRegistrationBean getServlet(){
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 
 }
